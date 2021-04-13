0647086122c97ee3f26bea69f5aecb89211fde46
dimon78 <72882918+dimon78@users.noreply.github.com>
2021-04-13 18:17:34 +0300

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+obj.set('wl_relay_1', 0)
+obj.set('wl_relay_2', 0)
+obj.set('wl_relay_3', 0)
+
+obj.set('master', 0)
+obj.set('master_trigger', 0)
+obj.set('service', os.time() + 86400)
+obj.set('service_trigger', 0)
